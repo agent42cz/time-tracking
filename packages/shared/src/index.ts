9ec9f7ab19dcf8451032a59c61b54fe7df971183
Michal Lénert <misalenert@gmail.com>
2026-07-27 15:49:35 +0000

    fix(shared): re-export darkVariantOf and CLIENT_COLOR_VALUES from the barrel (AIAGE-57)

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,4 +1,11 @@
-export { CLIENT_COLORS, DEFAULT_CLIENT_COLOR, isClientColor } from './colors.js';
+export {
+  CLIENT_COLOR_VALUES,
+  CLIENT_COLORS,
+  DEFAULT_CLIENT_COLOR,
+  darkVariantOf,
+  isClientColor,
+} from './colors.js';
+export type { ClientColor } from './colors.js';
 export * from './time/index.js';
 export * from './validators/index.js';
 export * from './ws/index.js';
